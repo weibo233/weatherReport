76c5b7f2b7da3286ea96cde5cf13ca27f99e03bd
weibo233 <wujiandong856>
2021-12-14 23:10:20 +0800

    页面功能实现

--- App.js
+++ App.js
@@ -15,7 +15,11 @@ window.onload = function () {
     router,
     data: {
       cityOrCode: "",
-      eys: "cc1269769bd442e6bed442f41f27c17a"
+      key: "cc1269769bd442e6bed442f41f27c17a",
+      updateTime:"",
+      now:{},
+      name:"",
+      activeIndex:1
     },
     mounted() {},
     methods: {
@@ -26,7 +30,7 @@ window.onload = function () {
         }
         axios
           .get(
-            `https://geoapi.qweather.com/v2/city/lookup?location=${queryString}&key=${this.eys}`
+            `https://geoapi.qweather.com/v2/city/lookup?location=${queryString}&key=${this.key}`
           )
           .then((res) => {
             let { code, location } = res.data;
@@ -48,19 +52,25 @@ window.onload = function () {
           });
       },
       handleSelect(item) {
+        this.name = item.name
         axios
           .get(
-            `https://devapi.qweather.com/v7/weather/now?location=${item.id}&key=${this.eys}`
+            `https://devapi.qweather.com/v7/weather/now?location=${item.id}&key=${this.key}`
           )
           .then((res) => {
-            let { code, now } = res.data;
+            let { code, now ,updateTime} = res.data;
             if (code == 200) {
+              this.updateTime = dayjs(updateTime).format('HH:mm:ss');
+              this.now = now
               console.log(now, "now");
             }
           });
 
         console.log(item, "item");
       },
+      handleSelectOptions(item) {
+        console.log(item,"item")
+      }
     },
   }).$mount("#app");
 };
